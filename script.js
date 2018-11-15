84b329c1bcae67f6d406eea9e89b4eac7457a563
Nielene Thomas <nielenethomas@pursuit.org>
2018-11-14 20:04:38 -0500

    5th commit

--- script.js
+++ script.js
@@ -122,23 +122,76 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
+  let breedList = [];
+
+  let breed = APIResponse.message;
+  Object.keys(breed).forEach(subBreed => {
+
+    if (breed[subBreed].length) {
+      breed[subBreed].forEach(type => {
+
+        let first = type[0].toUpperCase()
+        // console.log("firstTop", first);
+        let end = type.slice(1);
+        // console.log("endTop", end);
+
+        let firstSub = subBreed[0].toUpperCase();
+        let endSub = subBreed.slice(1);
+
+
+
+        breedList.push(`${first + end} ${firstSub + endSub}`);
+        // breedList.push(first + end + " " + firstSub + endSub);
+        // breedList.push(type + " " + subBreed)
+
+        // console.log(type + " " + subBreed);
+      });
+    } else {
+      let first = subBreed[0].toUpperCase();
+      let end = subBreed.slice(1)
+      // console.log("first",first, "end:",end)
+      breedList.push(`${first+end}`);
+      // breedList.push(subBreed);
+      // console.log(subBreed);
+    }
+  });
+// console.log(breedList);
+// let myArr = [10,20,30,40,50];
+// let num = Math.random();
+const randomNum = (length) => {
+  return Math.floor(Math.random() *length)
+}
+const getElement = (arr) => {
+  let index = randomNum(arr.length)
+  return arr[index];
+}
+
+console.log(getElement(breedList));
+
   let button = document.querySelector("button");
 
   button.addEventListener("click", event => {
-    let subBreedArr = APIResponse.message;
-    // console.log("APIResponse.message", subBreedArr);
+//on click: randomize the list i already created. and push it to p tag
 
-    Object.keys(subBreedArr).forEach(subBreed => {
-      // console.log("Object.keys(APIResponse.message)", Object.keys(subBreedArr));
-      // console.log("object.values(APIResponse.message)", Object.values(subBreedArr));
+  let body = document.querySelector("body");
+// let p = document.createElement("p");
 
-      if (subBreedArr[subBreed].length) {
-        subBreedArr[subBreed].forEach(type => {
-          console.log(type + " " + subBreed);
-        });
-      } else {
-        console.log(subBreed);
-      }
-    });
+// body.appendChild(p);
+  // return p.innerText;
+    // p.innerText = subBreed;
+    // p.innerText = type + " " + subBreed;
+
+
+
+
+
   });
 });
+
+
+//-----------------------------------
+
+// console.log("APIResponse.message", Breed);
+
+// console.log("Object.keys(APIResponse.message)", Object.keys(Breed));
+// console.log("object.values(APIResponse.message)", Object.values(Breed));
